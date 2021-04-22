f555a74d69f9ebd2d7eb9f7cbd0a8c5861b61c31
Armando Elias Aguirre Sosa <armandoaguirre1993@gmail.com>
2021-04-22 13:09:15 -0600

    cambios

---
+++ index.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch');
+
+function temporizador(){
+    fetch("http://localhost:3000/api/fechahora")
+        .then(promesa => promesa.json())
+        .then(contenido => console.log(contenido));
+        
+}
+ setInterval(temporizador, 5000);
+
+ console.log("Hola");
--- src/client/index.js
+++
@@ -1,5 +0,0 @@
-const fetch = require('node-fetch');
-
-fetch("http://localhost:3000/api/fechahora")
-    .then(promesa => promesa.json())
-    .then(contenido => console.log(contenido));
